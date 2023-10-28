9934f287c4592e5408eb625df2a17b68c5fbde71
Carenalga <mapedorr@gmail.com>
2023-10-28 05:40:03 -0500

    Until step 04

---
+++ popochiu/autoloads/a.gd
@@ -0,0 +1,18 @@
+@tool
+extends "res://addons/popochiu/engine/interfaces/i_audio.gd"
+
+# cues ----
+var vo_goddiu_happy_01: AudioCueSound = load("res://popochiu/characters/goddiu/audio/vo_goddiu_happy_01.tres")
+var vo_goddiu_happy_02: AudioCueSound = load("res://popochiu/characters/goddiu/audio/vo_goddiu_happy_02.tres")
+var vo_goddiu_happy_03: AudioCueSound = load("res://popochiu/characters/goddiu/audio/vo_goddiu_happy_03.tres")
+var vo_goddiu_normal_01: AudioCueSound = load("res://popochiu/characters/goddiu/audio/vo_goddiu_normal_01.tres")
+var vo_goddiu_normal_02: AudioCueSound = load("res://popochiu/characters/goddiu/audio/vo_goddiu_normal_02.tres")
+var vo_goddiu_normal_03: AudioCueSound = load("res://popochiu/characters/goddiu/audio/vo_goddiu_normal_03.tres")
+var vo_popsy_happy_01: AudioCueSound = load("res://popochiu/characters/popsy/audio/vo_popsy_happy_01.tres")
+var vo_popsy_happy_02: AudioCueSound = load("res://popochiu/characters/popsy/audio/vo_popsy_happy_02.tres")
+var vo_popsy_happy_03: AudioCueSound = load("res://popochiu/characters/popsy/audio/vo_popsy_happy_03.tres")
+var vo_popsy_normal_03: AudioCueSound = load("res://popochiu/characters/popsy/audio/vo_popsy_normal_03.tres")
+var vo_popsy_normal_04: AudioCueSound = load("res://popochiu/characters/popsy/audio/vo_popsy_normal_04.tres")
+var vo_popsy_normal_05: AudioCueSound = load("res://popochiu/characters/popsy/audio/vo_popsy_normal_05.tres")
+# ---- cues
+
---
+++ popochiu/rooms/house/room_house.gd
@@ -0,0 +1,41 @@
+@tool
+extends PopochiuRoom
+
+const Data := preload('room_house_state.gd')
+
+var state: Data = load('res://popochiu/rooms/house/room_house.tres')
+
+
+# ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ GODOT ░░░░
+# TODO: Overwrite Godot's methods
+
+
+# ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ VIRTUAL ░░░░
+# What happens when Popochiu loads the room. At this point the room is in the
+# tree but it is not visible
+func _on_room_entered() -> void:
+	pass
+
+
+# What happens when the room changing transition finishes. At this point the room
+# is visible.
+func _on_room_transition_finished() -> void:
+	await C.Goddiu.say("Hi")
+	await C.Popsy.say("Hi ya!")
+	await C.Goddiu.say("We are the [rainbow]GodotCon[/rainbow]!!!")
+	await C.Popsy.say("[shake]I CAN'T BELIVE IT[/shake]")
+
+
+# What happens before Popochiu unloads the room.
+# At this point, the screen is black, processing is disabled and all characters
+# have been removed from the $Characters node.
+func _on_room_exited() -> void:
+	pass
+
+
+# ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ PUBLIC ░░░░
+# You could put public functions here
+
+
+# ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ PRIVATE ░░░░
+# You could put private functions here
